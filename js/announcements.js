4fca373fc3695adfdcf8409bbe8a6e30f2c8b488
Dyllan Cole <dyllancole@outlook.com>
2018-05-21 08:26:34 -0400

    Comment announcements.js.

--- js/announcements.js
+++ js/announcements.js
@@ -1,6 +1,8 @@
+// The title and content elements for the main announcement pane.
 let titleHeader;
 let subParagraph;
 
+// Arrays to hold separate announcement categories until they are all retrieved.
 let mainTitles = [];
 let mainContents = [];
 let libraryTitles = [];
@@ -8,34 +10,44 @@ let libraryContents = [];
 let calendarTitles = [];
 let calendarContents = [];
 
+// Booleans indicating whether announcement categories have been retrieved.
 let filledMain = false;
 let filledLibrary = false;
 let filledCalendar = false;
 
+// The final arrays to hold the announcements.
 let titles = [];
 let contents = [];
+// Index of currently displayed announcement.
 let announcementIndex = 0;
+// A switch set by the showlibrary GET parameter that determines whether library announcements are added in.
 let showLibrary = false;
 
+// Google API key.
 const API_KEY = "AIzaSyCzjAj9QlD1P_eG_1HT7KqQjbfmfSDw-TU";
 const DISCOVERY_DOCS = ["https://content.googleapis.com/discovery/v1/apis/sheets/v4/rest", "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
+// Load the google api client and authenticate.
 function loadClient() {
     return gapi.client.init({
         apiKey: API_KEY,
         discoveryDocs: DISCOVERY_DOCS,
-    })
-        .then(function () {
-                updateArrays();
-                setInterval(cycleAnnouncements, 5 * 1000);
-                setInterval(updateArrays, 5 * 60 * 1000);
-            },
-            function (err) {
-                console.error("Error loading GAPI client for API", err);
-            });
+    }).then(function () {
+            // Initially populate the announcement arrays.
+            updateArrays();
+            // Cycle the announcements every 5 seconds.
+            setInterval(cycleAnnouncements, 5 * 1000);
+            // Update the announcements every 30 minutes.
+            setInterval(updateArrays, 5 * 60 * 1000);
+        },
+        function (err) {
+            console.error("Error loading GAPI client for API", err);
+        });
 }
 
+// Fill the final announcement arrays.
 function populateAnnouncements() {
+    // We fill in the order Main, [Library], Calendar.
     titles = mainTitles;
     contents = mainContents;
 
@@ -47,6 +59,8 @@ function populateAnnouncements() {
     titles = titles.concat(calendarTitles);
     contents = contents.concat(calendarContents);
 
+    // Reset the temporary announcement arrays to avoid holdovers.
+
     mainTitles = [];
     mainContents = [];
     libraryTitles = [];
@@ -55,9 +69,10 @@ function populateAnnouncements() {
     calendarContents = [];
 }
 function processError(err) {
-    console.error("Execute error", err);
+    console.error("Execution error.", err);
 }
 
+// Runs recursively until all the announcement data has been retrieved, so that we don't get partial stuff.
 function finalize() {
     if (filledMain && filledLibrary && filledCalendar) {
         filledMain = false;
@@ -71,10 +86,13 @@ function finalize() {
     }
 }
 
+// Fill the announcement data arrays.
 function updateArrays() {
+    // Reset to avoid holdovers.
     titles = [];
     contents = [];
 
+    // Retrieve the main announcements from the spreadsheet.
     gapi.client.sheets.spreadsheets.values.get({
         "spreadsheetId": "1Vlo2zrJM6Hz05T4_ux96p4EhaQVX9p_WJ0xMw3Lsj6s",
         "range": "A1:B",
@@ -109,6 +127,7 @@ function updateArrays() {
     }, processError);
 
     if (showLibrary) {
+        // Retrieve the library announcements from the spreadsheet.
         setTimeout(gapi.client.sheets.spreadsheets.values.get({
             "spreadsheetId": "1Vlo2zrJM6Hz05T4_ux96p4EhaQVX9p_WJ0xMw3Lsj6s",
             "range": "'Library Announcements'!A1:B",
@@ -144,11 +163,14 @@ function updateArrays() {
         }, processError), 100);
     }
 
+    // Retrieve calendar events from the public school calendar.
     updateCalendarEvents();
 
+    // Run finalize() until we get all the data we need.
     setTimeout(finalize, 500);
 }
 
+// Retrieve calendar events from the school calendar.
 function updateCalendarEvents() {
 
     let day = now.getDate();
@@ -209,6 +231,7 @@ function updateCalendarEvents() {
     });
 }
 
+// Cycle through the announcements in the main pane.
 function cycleAnnouncements() {
     if (mainTitles.length !== 0 || mainContents.length !== 0
     || libraryTitles.length !== 0 || libraryContents.length !== 0
@@ -223,14 +246,17 @@ function cycleAnnouncements() {
 }
 
 window.addEventListener("load", function () {
+    // This is for retrieving the GET parameters.
     let queryDict = {};
     location.search.substr(1).split("&").forEach(function (item) {
         queryDict[item.split("=")[0]] = item.split("=")[1]
     });
 
+    // Be a bit lenient on the capitalization.
     if (queryDict["showlibrary"] === "true") showLibrary = true;
     if (queryDict["showLibrary"] === "true") showLibrary = true;
 
+    // Set the elements only once the window is loaded.
     titleHeader = document.getElementById("announcementTitle");
     subParagraph = document.getElementById("announcementBody");
 });
